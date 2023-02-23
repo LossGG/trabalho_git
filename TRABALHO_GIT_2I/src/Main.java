df069143a22342400a75d5b0506046013f3049c6
"Lucas Gabriel" <lucasaraujo.ss042@academico.ifsul.edu.br>
2023-02-23 11:41:34 -0300

    Funcionalidade digitar nome e imprimir na tela

--- TRABALHO_GIT_2I/src/Main.java
+++ TRABALHO_GIT_2I/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("ola mundo");
+		Scanner t = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
+		String a = t.nextLine();
+		System.out.println("ola " + a);
+		System.out.println("ola mundo");
 	}
 
 }
